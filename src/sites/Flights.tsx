c9c8bb1ee67e5f16b3961d2e3f976225cd26bff0
CinekTV <cinekv15@gmail.com>
2025-06-09 12:05:07 +0200

    Front-start

--- src/sites/Flights.tsx
+++ src/sites/Flights.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "../components/Header/Header.tsx";
+import apiCalls from "../api.tsx";
 
 const Flights:React.FC = () => {
+
+    const [flights, setFlights] = useState([]);
+    useEffect(() => {
+        apiCalls.getFlights()
+            .then(data => setFlights(data))
+            .catch(err => console.error(err));
+    }, []);
+
     return(
         <>
             <Header/>
-            Tutaj będzie lista lotów
+            <ul>
+                {flights.map((flight: any) => (
+                    <li key={flight._id}>
+                        {flight.fromCity} → {flight.toCity} ({flight.date})
+                    </li>
+                ))}
+            </ul>
         </>
     );
 }
